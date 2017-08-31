7774ce07712d3ad159c4f72279e67ef5a52df947
Gamal Al-Hetari <gn62192@gmail.com>
2017-08-31 20:07:46 +0000

    quiz 2 complete

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,4 +2,24 @@ class GamesController < ApplicationController
     def index
         @games=Game.all
     end
+    
+    def new
+        @game = Game.new
+    end
+    
+    def create
+        @game = Game.create(game_params)
+        
+        if @game.valid?
+            redirect_to root_path
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+    
+    private
+    
+    def game_params
+        params.require(:game).permit(:name, :description)
+    end
 end
